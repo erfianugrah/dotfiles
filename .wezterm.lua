6593d3e9678431ba02b8a211f46e7bb41d52d013
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-24 07:29:41 +0000

    linux native config

---
+++ .wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "lovelace"
+-- config.term = "xterm-265color"
+
+-- WSL
+--config.default_domain = "WSL:Ubuntu"
+
+-- Fonts
+-- config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 10
+config.line_height = 1
+
+-- Paste
+
+-- Working directory
+config.default_cwd = "/home/erfi"
+
+-- and finally, return the configuration to wezterm
+return config
