b9554f759684d6ae6503d6b89d65a9b86b4c3b5e
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-27 14:26:51 +0000

    wezterm config edit

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,19 +8,29 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = "lovelace"
+-- config.term = "xterm-265color"
 
--- WSL
--- config.default_domain = "WSL:Ubuntu"
+-- Windows
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- WSL
+	config.default_domain = "WSL:Ubuntu"
+	config.default_cwd = "/home/erfi"
+	config.launch_menu = {
+		{
+			label = "PowerShell",
+			args = { "powershell.exe", "-NoLogo" },
+		},
+	}
+end
 
+-- Keybind for launch_menu
+config.keys = {
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+}
 -- Fonts
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 10
+config.font = wezterm.font("IosevkaTerm NF")
+config.font_size = 11
 config.line_height = 1
-
--- Paste
-
--- Working directory
-config.default_cwd = "/home/deck"
 
 -- and finally, return the configuration to wezterm
 return config
