108d6dc8d7f965469c8bf8c2518857aab62f335b
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-28 07:55:28 +0000

    wezterm gpu

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,13 +3,23 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
 config.color_scheme = "lovelace"
 
 -- Windows
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- Test GPU
+	config.webgpu_preferred_adapter = {
+		backend = "Vulkan",
+		device = 8712,
+		device_type = "DiscreteGpu",
+		driver = "NVIDIA",
+		driver_info = "552.12",
+		name = "NVIDIA GeForce RTX 3080 Ti",
+		vendor = 4318,
+	}
+	config.front_end = "WebGpu"
 	-- WSL
 	config.default_domain = "WSL:Ubuntu"
 	config.default_cwd = "/home/erfi"
