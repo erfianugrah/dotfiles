d01b362ff5a8b8f179bed3cd307680fe1cd5f96b
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-30 08:05:33 +0000

    steady bar and brew list

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,7 +13,7 @@ config.initial_rows = 40
 config.initial_cols = 160
 
 -- Cursor
-config.default_cursor_style = "BlinkingBar"
+config.default_cursor_style = "SteadyBar"
 
 -- FPS
 config.animation_fps = 120
